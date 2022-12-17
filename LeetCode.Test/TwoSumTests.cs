f202cc2abcc677d4b443922bea5ff12b0406710c
Anthea Wu <anthea@emct.com.tw>
2022-12-17 16:01:32 +0800

    Two Sum
    - [x] Accept
    - Runtime 172ms
    - Memory 44.2MB

--- LeetCode.Test/TwoSumTests.cs
+++ LeetCode.Test/TwoSumTests.cs
@@ -7,21 +7,32 @@ namespace LeetCode.Test;
 public class TwoSumTests
 {
     [TestCaseSource(nameof(TwoSumTestCasesData))]
-    public void return_correct_index(int[] input, int target)
+    public void return_correct_index_another_method(int[] input, int target, int firstIndex, int secondIndex)
+    {
+        var twoSum = new TwoSum();
+        var indexes = twoSum.DoAnother(input, target);
+
+        Assert.AreEqual(firstIndex, indexes[0]);
+        Assert.AreEqual(secondIndex, indexes[1]);
+    }
+    
+    [TestCaseSource(nameof(TwoSumTestCasesData))]
+    public void return_correct_index(int[] input, int target, int firstIndex, int secondIndex)
     {
         var twoSum = new TwoSum();
         var indexes = twoSum.Do(input, target);
         
-        Assert.AreEqual(target, input[indexes[0]] + input[indexes[1]]);
+        Assert.AreEqual(firstIndex, indexes[0]);
+        Assert.AreEqual(secondIndex, indexes[1]);
     }
 
     public static IEnumerable TwoSumTestCasesData
     {
         get
         {
-            yield return new TestCaseData(new[]{2,7,11,15}, 9);
-            yield return new TestCaseData(new[]{3,2,4}, 6);
-            yield return new TestCaseData(new[]{3,3}, 6);
+            yield return new TestCaseData(new[]{2,7,11,15}, 9, 0, 1);
+            yield return new TestCaseData(new[]{3,2,4}, 6, 1, 2);
+            yield return new TestCaseData(new[]{3,3}, 6, 0, 1);
         }
     }
 }
--- LeetCode/TwoSum.cs
+++ LeetCode/TwoSum.cs
@@ -17,4 +17,20 @@ public class TwoSum
 
         return new int[] { };
     }
+
+    public int[] DoAnother(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var remain = target - nums[i];
+            var indexOfRemain = Array.IndexOf(nums, remain, i + 1);
+            
+            if (indexOfRemain >= 0 && indexOfRemain != i)
+            {
+                return new []{i, indexOfRemain};
+            }
+        }
+
+        return new int[] { };
+    }
 }
