a7cb83f07133dc3eaa7878457ca42f39384a2bc6
Anthea Wu <anthea@emct.com.tw>
2022-12-17 15:38:25 +0800

    Two Sum
    - [x] Accept
    - Runtime 210ms
    - Memory 44.1MB

---
+++ LeetCode.Test/TwoSumTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace LeetCode.Test;
+
+[TestFixture]
+public class TwoSumTests
+{
+    [TestCaseSource(nameof(TwoSumTestCasesData))]
+    public void return_correct_index(int[] input, int target)
+    {
+        var twoSum = new TwoSum();
+        var indexes = twoSum.Do(input, target);
+        
+        Assert.AreEqual(target, input[indexes[0]] + input[indexes[1]]);
+    }
+
+    public static IEnumerable TwoSumTestCasesData
+    {
+        get
+        {
+            yield return new TestCaseData(new[]{2,7,11,15}, 9);
+            yield return new TestCaseData(new[]{3,2,4}, 6);
+            yield return new TestCaseData(new[]{3,3}, 6);
+        }
+    }
+}
---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode;
+
+public class TwoSum
+{
+    public int[] Do(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (i == j) continue;
+
+                var sum = nums[i] + nums[j];
+                if (sum == target) return new[] { i, j };
+            }
+        }
+
+        return new int[] { };
+    }
+}
