adc1e87a3e19e2e2c0320079517cd8ec33de685b
Anthea Wu <anthea@emct.com.tw>
2022-12-18 21:57:01 +0800

    Failed: Valid Parentheses with '{[]}'

---
+++ LeetCode.Test/ValidParenthesesTests.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Test;
+
+[TestFixture]
+public class ValidParenthesesTests
+{
+    private ValidParentheses _validParentheses;
+
+    [SetUp]
+    public void Setup()
+    {
+        _validParentheses = new ValidParentheses();
+    }
+
+    [Test]
+    public void is_not_valid()
+    {
+        var s = "(]";
+
+        var isValid = _validParentheses.DoFirstTime(s);
+        
+        Assert.IsFalse(isValid);
+    }
+    
+    [Test]
+    public void is_valid()
+    {
+        var s = "()";
+        
+        var isValid = _validParentheses.DoFirstTime(s);
+        
+        Assert.IsTrue(isValid);
+    }    
+}
---
+++ LeetCode/ValidParentheses.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode;
+
+public class ValidParentheses
+{
+    public bool DoFirstTime(string s)
+    {
+        var firstParentheses = new Dictionary<string, string>()
+        {
+            {"(", ")"},
+            {"[", "]"},
+            {"{", "}"},
+        };
+        var secondParentheses = new Dictionary<string, string>()
+        {
+            {")", "("},
+            {"]", "["},
+            {"}", "{"},
+        };
+        for (var index = 0; index < s.Length / 2; index += 2)
+        {
+            var firstString = s.Substring(index, 1);
+            var isFirstExisted = firstParentheses.TryGetValue(firstString, out _);
+            if (isFirstExisted)
+            {
+                var secondString = s.Substring(index + 1, 1);
+                var isSecondExisted = secondParentheses.TryGetValue(secondString, out var expected);
+                return isSecondExisted && expected == firstString;
+            }
+        }
+        return false;
+    }
+}
