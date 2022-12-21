ce19848b8ba811013d6a6dc7fff09030cbe7fa29
Anthea Wu <anthea@emct.com.tw>
2022-12-21 18:38:32 +0800

    Success: 704 Binary Search - Runtime 134 ms (80.24%) / Memory 50.5 MB (25.24%)

---
+++ LeetCode.Test/BinarySearchTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace LeetCode.Test;
+
+[TestFixture]
+public class BinarySearchTests
+{
+    [TestCaseSource(nameof(DoSearchSuccessTestCasesData))]
+    public void do_search_success(int[] nums, int target, int expected)
+    {
+        var binarySearch = new BinarySearch();
+
+        var result = binarySearch.Do(nums, target);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    public static IEnumerable DoSearchSuccessTestCasesData
+    {
+        get
+        {
+            yield return new TestCaseData(new[] { -1, 0, 3, 5, 9, 12 }, 9, 4);
+            yield return new TestCaseData(new[] { -1, 0, 3, 5, 9, 12 }, 2, -1);
+            yield return new TestCaseData(new[] { -1, 0, 3, 5, 9, 12 }, 12, 5);
+            yield return new TestCaseData(new[] { -1, 0, 3, 5, 9, 12 }, -1, 0);
+        }
+    }
+}
---
+++ LeetCode/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode;
+
+public class BinarySearch
+{
+    public int Do(int[] nums, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+
+        while (leftIndex <= rightIndex)
+        {
+            var middle = (rightIndex - leftIndex) / 2 + leftIndex;
+
+            if (nums[middle] < target)
+            {
+                leftIndex = middle + 1;
+            }
+            else if (target < nums[middle])
+            {
+                rightIndex = middle - 1;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+        
+        return -1;
+    }
+}
