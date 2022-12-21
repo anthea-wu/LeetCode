9a97b92d458b8ee65e729f04e5f7708eca5de037
Anthea Wu <anthea@emct.com.tw>
2022-12-21 11:59:33 +0800

    Success: 21 Merge Two Sorted Lists - Runtime 81 ms (97.56%) / Memory 40 MB (7.66%)

---
+++ LeetCode.Test/MergeTwoSortedListsTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace LeetCode.Test;
+
+[TestFixture]
+public class MergeTwoSortedListsTests
+{
+    private MergeTwoSortedLists _mergeTwoSortedLists;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mergeTwoSortedLists = new MergeTwoSortedLists();
+    }
+
+    [TestCaseSource(nameof(MergeTwoSortedListsFirstTimeTestCasesData))]
+    public void do_first_time(List<int> ints1, List<int> ints2, List<int> expected)
+    {
+        var listNodes1 = CreateListNodes(ints1);
+        var listNodes2 = CreateListNodes(ints2);
+
+        var listNodes = _mergeTwoSortedLists.DoFirstTime(listNodes1, listNodes2);
+        var result = GetAllNodes(listNodes);
+        Assert.AreEqual(expected.Count, result.Count);
+        for (int i = 0; i < result.Count; i++)
+        {
+            Assert.AreEqual(expected[i], result[i]);
+        }
+    }
+
+    private List<int> GetAllNodes(ListNode listNodes)
+    {
+        var list = new List<int>();
+        while (listNodes != null)
+        {
+            list.Add(listNodes.val);
+            listNodes = listNodes.next;
+        }
+        return list;
+    }
+
+    private ListNode CreateListNodes(List<int> ints)
+    {
+        ListNode lastListNode = null;
+        for (var i = ints.Count - 1; i > -1; i--)
+        {
+            lastListNode = new ListNode(ints[i], lastListNode);
+        }
+
+        return lastListNode;
+    }
+
+    public static IEnumerable MergeTwoSortedListsFirstTimeTestCasesData
+    {
+        get
+        {
+            yield return new TestCaseData(new List<int>(){ 1,2,4 }, new List<int>(){ 1,3,4 }, new List<int>(){ 1, 1, 2, 3, 4, 4 });
+            yield return new TestCaseData(new List<int>(){}, new List<int>(){}, new List<int>(){});
+            yield return new TestCaseData(new List<int>(){}, new List<int>(){ 0 }, new List<int>(){ 0 });
+        }
+    }
+}
---
+++ LeetCode/MergeTwoSortedLists.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode;
+
+public class MergeTwoSortedLists
+{
+    public ListNode DoFirstTime(ListNode list1, ListNode list2)
+    {
+        if (list1 == null && list2 == null) return null;
+        
+        var min = GetMinNode(ref list1, ref list2);
+        var head = new ListNode(min);
+        var current = head;
+        while (list1 != null || list2 != null)
+        {
+            var nextMin = GetMinNode(ref list1, ref list2);
+            current.next = new ListNode(nextMin);
+            current = current.next;
+        }
+        
+        return head;
+    }
+
+    private static int GetMinNode(ref ListNode list1, ref ListNode list2)
+    {
+        int min;
+        if (list1 == null)
+        {
+            min = list2.val;
+            list2 = list2.next;
+        }
+        else if (list2 == null)
+        {
+            min = list1.val;
+            list1 = list1.next;
+        }
+        else if (list1.val < list2.val)
+        {
+            min = list1.val;
+            list1 = list1.next;
+        }
+        else
+        {
+            min = list2.val;
+            list2 = list2.next;
+        }
+
+        return min;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
