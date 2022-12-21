e9a57facea87394c6fbec4c6af573c78df3ddb35
Anthea Wu <anthea@emct.com.tw>
2022-12-21 19:15:09 +0800

    Success: Remove Element - O(n^2) / O(1)

---
+++ LeetCode.Test/RemoveElementTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace LeetCode.Test;
+
+[TestFixture]
+public class RemoveElementTests
+{
+    private RemoveElement _removeElement;
+
+    [SetUp]
+    public void Setup()
+    {
+        _removeElement = new RemoveElement();
+    }
+
+    [TestCaseSource(nameof(DoFirstTimeTestCasesData))]
+    public void do_first_time_success(int[] nums, int val, int[] expected)
+    {
+        var result = _removeElement.DoFirstTime(nums, val);
+        
+        Assert.AreEqual(expected.Length, result);
+        for (var i = 0; i < expected.Length; i++)
+        {
+            Assert.AreEqual(expected[i], nums[i]);            
+        }
+    }
+
+    public static IEnumerable DoFirstTimeTestCasesData
+    {
+        get
+        {
+            yield return new TestCaseData(new int[] { 3, 2, 2, 3 }, 3, new int[] { 2, 2 });
+            yield return new TestCaseData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, new int[] { 0, 1, 3, 0, 4 });
+        }
+    }
+}
---
+++ LeetCode/RemoveElement.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode;
+
+public class RemoveElement
+{
+    public int DoFirstTime(int[] nums, int val)
+    {
+        var size = nums.Length;
+        for (var i = 0; i < size; i++)
+        {
+            if (nums[i] == val)
+            {
+                for (var j = i + 1; j < size; j++)
+                {
+                    nums[j - 1] = nums[j];
+                }
+
+                i--;
+                size--;
+            }
+        }
+
+        return size;
+    }
+}
