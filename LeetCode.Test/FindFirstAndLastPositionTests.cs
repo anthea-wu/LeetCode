cc1fb3489204700eab5d8a33fac4835b1a7a767f
Anthea Wu <anthea@emct.com.tw>
2022-12-23 23:34:54 +0800

    Success: 34. Find First and Last Position of Element in Sorted Array - O(log n) / O(1)

---
+++ LeetCode.Test/FindFirstAndLastPositionTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace LeetCode.Test;
+
+[TestFixture]
+public class FindFirstAndLastPositionTests
+{
+    [TestCaseSource(nameof(FailedTestCasesData))]
+    public void do_first_time_failed(int[] nums, int target)
+    {
+        var findFirstAndLastPosition = new FindFirstAndLastPosition();
+        var result = findFirstAndLastPosition.Do(nums, target);
+        Assert.AreEqual(-1, result[0]);
+        Assert.AreEqual(-1, result[1]);
+    }
+    
+    [TestCaseSource(nameof(SuccessTestCasesData))]
+    public void do_first_time_success(int[] nums, int target, int expectedFirstIndex, int expectedSecondIndex)
+    {
+        var findFirstAndLastPosition = new FindFirstAndLastPosition();
+        var result = findFirstAndLastPosition.Do(nums, target);
+        Assert.AreEqual(expectedFirstIndex, result[0]);
+        Assert.AreEqual(expectedSecondIndex, result[1]);
+    }
+
+    public static IEnumerable FailedTestCasesData
+    {
+        get
+        {
+            yield return new TestCaseData(Array.Empty<int>(), 0);
+            yield return new TestCaseData(new[] { 5, 7, 7, 8, 8, 10 }, 6);
+        }
+    }
+    
+    public static IEnumerable SuccessTestCasesData
+    {
+        get
+        {
+            yield return new TestCaseData(new[] { 5, 7, 7, 8, 8, 10 }, 8, 3, 4);
+        }
+    }
+}
---
+++ LeetCode/FindFirstAndLastPosition.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode;
+
+public class FindFirstAndLastPosition
+{
+    public int[] Do(int[] nums, int target)
+    {
+        var rightBorder = GetRightBorder(nums, target);
+        var leftBorder = GetLeftBorder(nums, target);
+        
+        if (rightBorder == -999 || leftBorder == -999) return new[] { -1, -1 };
+        if (rightBorder > leftBorder) return new[] { leftBorder + 1, rightBorder - 1 };
+
+        return new[] { -1, -1 };
+    }
+
+    private int GetLeftBorder(int[] nums, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+        var leftBorder = -999;
+
+        while (leftIndex <= rightIndex)
+        {
+            var middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+            if (target < nums[middleIndex])
+            {
+                rightIndex = middleIndex - 1;
+            }
+            else if (target > nums[middleIndex])
+            {
+                leftIndex = middleIndex + 1;
+            }
+            else
+            {
+                rightIndex = middleIndex - 1;
+                leftBorder = rightIndex;
+            }
+        }
+
+        return leftBorder;
+    }
+
+    private int GetRightBorder(int[] nums, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+        var rightBorder = -999;
+
+        while (leftIndex <= rightIndex)
+        {
+            var middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+            if (target < nums[middleIndex])
+            {
+                rightIndex = middleIndex - 1;
+            }
+            else if (target > nums[middleIndex])
+            {
+                leftIndex = middleIndex + 1;
+            }
+            else
+            {
+                leftIndex = middleIndex + 1;
+                rightBorder = leftIndex;
+            }
+        }
+
+        return rightBorder;
+    }
+}
