70ab94e3c2147fd12d7b26ac8503b241a1536ca4
Anthea Wu <anthea@emct.com.tw>
2022-12-24 15:09:48 +0800

    Success: 34. Find First and Last Position of Element in Sorted Array (Second Time) - O(log n) / O(1)

--- LeetCode.Test/FindFirstAndLastPositionTests.cs
+++ LeetCode.Test/FindFirstAndLastPositionTests.cs
@@ -6,6 +6,24 @@ namespace LeetCode.Test;
 public class FindFirstAndLastPositionTests
 {
     [TestCaseSource(nameof(FailedTestCasesData))]
+    public void do_second_time_failed(int[] nums, int target)
+    {
+        var findFirstAndLastPosition = new FindFirstAndLastPosition();
+        var result = findFirstAndLastPosition.DoSecondTime(nums, target);
+        Assert.AreEqual(-1, result[0]);
+        Assert.AreEqual(-1, result[1]);
+    }
+    
+    [TestCaseSource(nameof(SuccessTestCasesData))]
+    public void do_second_time_success(int[] nums, int target, int expectedFirstIndex, int expectedSecondIndex)
+    {
+        var findFirstAndLastPosition = new FindFirstAndLastPosition();
+        var result = findFirstAndLastPosition.DoSecondTime(nums, target);
+        Assert.AreEqual(expectedFirstIndex, result[0]);
+        Assert.AreEqual(expectedSecondIndex, result[1]);
+    }
+    
+    [TestCaseSource(nameof(FailedTestCasesData))]
     public void do_first_time_failed(int[] nums, int target)
     {
         var findFirstAndLastPosition = new FindFirstAndLastPosition();
--- LeetCode/FindFirstAndLastPosition.cs
+++ LeetCode/FindFirstAndLastPosition.cs
@@ -66,4 +66,71 @@ public class FindFirstAndLastPosition
 
         return rightBorder;
     }
+
+    public int[] DoSecondTime(int[] nums, int target)
+    {
+        var result = new[] { -1, -1 };
+
+        if (nums == null || nums.Length == 0) return result;
+        result[0] = GetStartIndex(nums, target);
+        result[1] = GetEndIndex(nums, target);
+
+        return result;
+    }
+
+    private int GetEndIndex(int[] nums, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+        var endIndex = -1;
+
+        while (leftIndex <= rightIndex)
+        {
+            var middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+
+            if (target < nums[middleIndex])
+            {
+                rightIndex = middleIndex - 1;
+            }
+            else if (target > nums[middleIndex])
+            {
+                leftIndex = middleIndex + 1;
+            }
+            else
+            {
+                endIndex = middleIndex;
+                leftIndex = middleIndex + 1;
+            }
+        }
+        
+        return endIndex;
+    }
+
+    private int GetStartIndex(int[] nums, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+        var startIndex = -1;
+
+        while (leftIndex <= rightIndex)
+        {
+            var middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+
+            if (target < nums[middleIndex])
+            {
+                rightIndex = middleIndex - 1;
+            }
+            else if (target > nums[middleIndex])
+            {
+                leftIndex = middleIndex + 1;
+            }
+            else
+            {
+                startIndex = middleIndex;
+                rightIndex = middleIndex - 1;
+            }
+        }
+        
+        return startIndex;
+    }
 }
