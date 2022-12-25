b70dc862b1030882ff53724f59c3e43bc5b13d78
Anthea Wu <anthea@emct.com.tw>
2022-12-25 22:04:06 +0800

    Success: 2515. Shortest Distance to Target String in a Circular Array - O(n^2) / O(n)

---
+++ LeetCode.Test/ShortestDistanceTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace LeetCode.Test;
+
+[TestFixture]
+public class ShortestDistanceTests
+{
+    private ShortestDistance _shortestDistance;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _shortestDistance = new ShortestDistance();
+    }
+
+    [TestCaseSource(nameof(ShortestDistanceTestCasesData))]
+    public void do_success(string[] words, string target, int startIndex, int expectedDistance)
+    {
+        var result = _shortestDistance.Do(words, target, startIndex);
+        Assert.AreEqual(expectedDistance, result);
+    }
+
+    public static IEnumerable ShortestDistanceTestCasesData
+    {
+        get
+        {
+            yield return new TestCaseData(new[] { "hello", "i", "am", "leetcode", "hello" }, "hello", 1, 1);
+            yield return new TestCaseData(new[] { "a", "b", "leetcode" }, "leetcode", 0, 1);
+            yield return new TestCaseData(new[] { "i", "eat", "leetcode" }, "ate", 0, -1);
+            yield return new TestCaseData(new[]
+            {
+                "hsdqinnoha", "mqhskgeqzr", "zemkwvqrww", "zemkwvqrww", "daljcrktje", "fghofclnwp", "djwdworyka",
+                "cxfpybanhd", "fghofclnwp", "fghofclnwp"
+            }, "zemkwvqrww", 8, 4);
+            yield return new TestCaseData(new[]
+            {
+                "pgmiltbptl", "jnkxwutznb", "bmeirwjars", "ugzyaufzzp", "pgmiltbptl", "sfhtxkmzwn", "pgmiltbptl",
+                "pgmiltbptl", "onvmgvjhxa", "jyzdtwbwqp"
+            }, "pgmiltbptl", 4, 0);
+            yield return new TestCaseData(new[]
+            {
+                "hqrdflmsiv", "bphfkwxwux", "jwhovjrktk", "masvwbrofg", "bpngempqkt", "bphfkwxwux", "bpngempqkt",
+                "wvpoegifyu", "bpngempqkt", "wrgdcgwvrx"
+            }, "bpngempqkt", 1, 3);
+        }
+    }
+}
---
+++ LeetCode/ShortestDistance.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCode;
+
+public class ShortestDistance
+{
+    public int Do(string[] words, string target, int startIndex)
+    {
+        if (words[startIndex] == target) return 0;
+        
+        var temp = 301;
+        var wordsLength = words.Length;
+        
+        var counter = 0;
+        var beforeCounter = 0;
+        var doubleWords = new string[wordsLength * 2 - 1];
+        for (var j = 0; j < 3; j++)
+        {
+            for (var i = 0; i < wordsLength; i++)
+            {
+                if (j == 0 && i > startIndex)
+                {
+                    doubleWords[counter] = words[i];
+                    beforeCounter++;
+                    counter++;
+                }
+                else if (j == 2 && i < startIndex)
+                {
+                    doubleWords[counter] = words[i];
+                    counter++;
+                }
+                else if (j == 1)
+                {
+                    doubleWords[counter] = words[i];
+                    counter++;
+                }
+            }
+        }
+
+        var startIndexInTripleWords = beforeCounter + startIndex;
+        for (var i = 0; i < doubleWords.Length; i++)
+        {
+            if (doubleWords[i] == doubleWords[startIndexInTripleWords]) continue;
+            if (doubleWords[i] == target)
+            {
+                if (temp == -1) temp = i;
+
+                var indexDistance = Math.Abs(i - startIndexInTripleWords);
+                if (indexDistance < temp) temp = indexDistance;
+            }
+        }
+        
+        for (var i = doubleWords.Length - 1; i >= 0; i--)
+        {
+            if (doubleWords[i] == doubleWords[startIndexInTripleWords]) continue;
+            if (doubleWords[i] == target)
+            {
+                if (temp == -1) temp = i;
+
+                var indexDistance = Math.Abs(doubleWords.Length - i);
+                if (indexDistance < temp) temp = indexDistance;
+            }
+        }
+
+        return temp > wordsLength ? -1 : temp;
+    }
+}
